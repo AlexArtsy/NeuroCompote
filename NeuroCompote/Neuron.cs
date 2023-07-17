b311ae2c921fe4432a97da4a76c8882c37121dab
Alex Arts <a_arz@mail.ru>
2023-07-17 17:28:55 +0400

    Create Synapse.cs and Axon.cs

--- NeuroCompote/Neuron.cs
+++ NeuroCompote/Neuron.cs
@@ -8,5 +8,40 @@ namespace NeuroCompote
 {
     internal class Neuron
     {
+        private double[] input;
+        private double Out;
+        private double[] w;
+        private double m;
+        private double pw;    // пороговый вес
+        private double delta;
+        private double sigma;
+        private double[,] historyOfW;
+        private double[] historyOfPW;
+
+        public Neuron(int valueOfInput, double sig, int nomer)
+        {
+            sigma = sig;
+            input = new double[valueOfInput];
+            Out = 0;
+            w = new double[valueOfInput];
+            Random rnd = new Random();
+            pw = rnd.NextDouble() / 100.0;
+
+            for (int i = 0; i < valueOfInput; i++)
+            {
+                w[i] = rnd.NextDouble() / 100.0;
+            }
+
+            if (nomer != 0)
+            {
+                historyOfW = new double[40000, valueOfInput];
+                historyOfPW = new double[40000];
+            }
+            else
+            {
+                historyOfW = new double[0, 0];
+                historyOfPW = new double[0];
+            }
+        }
     }
 }
