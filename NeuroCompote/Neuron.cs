28a6a6ef01a9bda2ac4cadb11e80388c19c6ff21
Alex Arts <a_arz@mail.ru>
2023-07-18 17:40:22 +0400

    Add constructors in Neuron

--- NeuroCompote/Neuron.cs
+++ NeuroCompote/Neuron.cs
@@ -9,11 +9,67 @@ namespace NeuroCompote
     internal class Neuron
     {
         private readonly int layerId;
-        public Sigmoid Sigma { get; set; }
-        public Neuron(int layerId, Sigmoid sigma)
+        public double InputValue { get; set; }
+        public double OutputValue { get; set; }
+        public IActivation ActivationFunc { get; set; }
+        private Synapse[] inputs;
+
+        #region Constructors
+        public Neuron(int inputValue, IActivation activationFunc)    //  Для входных нейронов.
+        {
+            InputValue = inputValue;
+            ActivationFunc = activationFunc;
+            OutputValue = CalculateValue(inputValue);
+        }
+
+        public Neuron(double inputValue, IActivation activationFunc)    //  Для входных нейронов.
+        {
+            InputValue = inputValue;
+            ActivationFunc = activationFunc;
+            OutputValue = CalculateValue(inputValue);
+        }
+
+        public Neuron(double[] inputs, IActivation activationFunc)
         {
             this.layerId = layerId;
-            Sigma = sigma;
+            ActivationFunc = activationFunc;
+            OutputValue = 0;
+            inputs = InitializeSynapse(valueOfInputs);
+        }
+        #endregion
+        private Synapse[] InitializeSynapse(int valueOfInputs, double inputValue)
+        {
+            var rnd = new Random();
+            var synapses = new Synapse[valueOfInputs];
+
+            for (var i = 0; i < valueOfInputs; i += 1)
+            {
+                var weight = rnd.NextDouble();
+                synapses[i] = new Synapse(weight, inputValue);
+            }
+            return synapses;
+        }
+        public double CalculateValue(int inputValue)
+        {
+
+            return 0;
+        }
+
+        public double CalculateValue(double inputValue)
+        {
+
+            return 0;
+        }
+
+        public double CalculateValue()
+        {
+
+            return 0;
+        }
+
+        private double GetWeightedSum()
+        {
+
         }
     }
 }
--- NeuroCompote/Sigmoid.cs
+++ NeuroCompote/Sigmoid.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace NeuroCompote
 {
-    internal class Sigmoid
+    internal class Sigmoid : IActivation
     {
+        private readonly double sigma;
+
+        public Sigmoid(double sigma)
+        {
+            this.sigma = sigma;
+        }
+
+        public double Calculate(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-1 * sigma * x));
+        }
+        
     }
 }
